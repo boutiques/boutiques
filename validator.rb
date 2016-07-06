3ec746e17da22c9974160d648438ed4efd379203
Tristan A.A <tristan.aumentado-armstrong@mail.mcgill.ca>
2016-07-06 17:01:35 -0400

    Fixing formatting

--- validator.rb
+++ validator.rb
@@ -38,8 +38,8 @@ end
 begin
   descriptor = JSON.parse( File.read(json_file) )
 rescue StandardError => e
-  puts e # if json is invalid, no need to check it
-  exit 1
+  puts e
+  exit 1 # if the json itself is invalid, no need to check it
 end
 
 ### Automatic descriptor validation with respect to schema structure ###
@@ -75,118 +75,85 @@ allIds.each_with_index do |s1,i|
   end
 end
 
-# IDs in groups, requires-inputs, and disables-inputs are present
-(descriptor['groups'] || []).map{ |g| g['members'].map{ |m| [g['id'],m] } }.flatten(1).each do |gid,mid|
-  errors.push("Member id #{mid} from group #{gid} is not present") unless inIds.include?(mid)
-end
-descriptor['inputs'].each do |d|
+## Checking inputs ##
+descriptor["inputs"].each do |v|
+
+  # Flag-type inputs always have command-line-flags, should not be required, and cannot be lists
+  if v["type"] == "Flag"
+    errors.push( "#{v["id"]} must have a command-line flag" ) unless v["command-line-flag"]
+    errors.push( "#{v["id"]} cannot be a list" ) if v["list"]
+    errors.push( "#{v["id"]} should not be required" ) if v["optional"]==false
+  # Number constraints (mins & maxs) are sensible
+  elsif v["type"] == "Number"
+    min, max = v["minimum"] || -1.0/0, v["maximum"] || 1.0/0
+    errors.push( "#{v['id']} cannot have greater min (#{min}) than max (#{max})" ) if min > max
+  # Enum-type inputs always have specified choices (at least 1), and the default must be in the choices set
+  elsif v["type"] == "Enum"
+    errors.push( "#{v['id']} must have at least one value choice" ) if (v["enum-value-choices"] || []).length < 1
+    badDefault = (ed = v["default-value"]).nil? ? false : !((v["enum-value-choices"] || []).include? ed)
+    errors.push( "#{v['id']} cannot have an default value outside its choices" ) if badDefault
+  end
+
+  # List length constraints are sensible
+  if v["list"]
+    min, max = v["min-list-entries"] || 0, v["max-list-entries"] || 1.0 / 0
+    errors.push( "#{v['id']} min list entries (#{min}) greater than max list entries (#{max})" ) if min > max
+    errors.push( "#{v['id']} cannot have negative min list entries #{min}" ) if min < 0
+    errors.push( "#{v['id']} cannot have non-positive max list entries #{max}" ) if max <= 0
+  # Non-list inputs cannot have the min/max-list-entries property
+  else
+    ['min','max'].each{ |r| errors.push("#{v['id']} can't use #{r}-list-entries") if v["#{r}-list-entries"] }
+  end
+
+  # IDs in requires-inputs and disables-inputs are present
   for s in ['require','disable']
-    (d[s + "s-inputs"] || []).each do |r|
-      errors.push( s.capitalize + "d id #{r} for #{d['id']} was not found" ) unless inIds.include?(r)
+    (v[s + "s-inputs"] || []).each do |r|
+      errors.push( s.capitalize + "d id #{r} for #{v['id']} was not found" ) unless inIds.include?(r)
     end
   end
-end
 
-# An input cannot both require and disable another input
-descriptor["inputs"].each do |ins|
-  for did in (ins["requires-inputs"] || [])
-    errors.push( "Id #{ins['id']} requires and disables #{did}" ) if (ins["disables-inputs"] || []).include?(did)
+  # An input cannot both require and disable another input
+  for did in (v["requires-inputs"] || [])
+    errors.push( "Id #{v['id']} requires and disables #{did}" ) if (v["disables-inputs"] || []).include?(did)
   end
+
 end
 
-# Group members cannot appear multiple times (in the same group or across groups)
-(descriptor["groups"] || []).each_with_index do |g1,gi|
-  g1['members'].each_with_index do |mcurr,i|
+## Checking Groups ##
+(descriptor["groups"] || []).each_with_index do |g,gi|
+
+  # Group members must exist in the inputs, but cannot appear multiple times (in the same group or across groups)
+  g['members'].each_with_index do |mcurr,i|
     (descriptor["groups"] || []).each_with_index do |g2,gj|
       g2['members'].each_with_index do |moth,j|
         unless gi > gj || (gi == gj && i >= j) # Prevent seeing the same error twice
-          errors.push( "#{mcurr} cannot appear twice (in groups #{g1["id"]} & #{g2["id"]})" ) if mcurr == moth
+          errors.push( "#{mcurr} cannot appear twice (in groups #{g["id"]} & #{g2["id"]})" ) if mcurr == moth
         end
       end
     end
+    errors.push("Member id #{mcurr} from group #{g['id']} is not present in the inputs") unless inIds.include?(mcurr)
   end
-end
 
-# Mutually exclusive groups cannot have members requiring other members, nor can they have required members
-(descriptor["groups"] || []).select{ |g| g["mutually-exclusive"] }.each do |g|
-  (mbrs = g["members"]).map{ |m| [m,inById.(m)] }.each do |id,m|
-    errors.push( "#{id} in mutex group #{g['id']} cannot be required" ) unless m['optional'] != false
-    for r in (m["requires-inputs"] || [])
-      errors.push( "#{id} in mutex group #{g['id']} cannot require fellow member " + r ) if mbrs.include?( r )
+  # Mutually exclusive groups cannot have members requiring other members, nor can they have required members
+  if g["mutually-exclusive"]
+    (mbrs = g["members"]).map{ |m| [m,inById.(m)] }.each do |id,m|
+      errors.push( "#{id} in mutex group #{g['id']} cannot be required" ) unless m['optional'] != false
+      for r in (m["requires-inputs"] || [])
+        errors.push( "#{id} in mutex group #{g['id']} cannot require fellow member " + r ) if mbrs.include?( r )
+      end
     end
   end
-end
 
-# One-is-Required groups should also never have required members (since it is automatically satisfied)
-(descriptor["groups"] || []).select{ |g| g["one-is-required"] }.each do |g|
-  g["members"].map{ |m| [m,inById.(m)] }.each do |id,m|
-    errors.push( "#{id} in one-is-required group #{g['id']} should not be required" ) unless m['optional'] != false
+  # One-is-Required groups should also never have required members (since it is automatically satisfied)
+  if g["one-is-required"]
+    g["members"].map{ |m| [m,inById.(m)] }.each do |id,m|
+      errors.push( "#{id} in one-is-required group #{g['id']} should not be required" ) unless m['optional'] != false
+    end
   end
-end
 
-# Flag-type inputs always have command-line-flags, should not be required, and cannot be lists
-descriptor["inputs"].select{ |v| v["type"] == "Flag" }.each do |f|
-  errors.push( "#{f["id"]} must have a command-line flag" ) unless f["command-line-flag"]
-  errors.push( "#{f["id"]} cannot be a list" ) if f["list"]
-  errors.push( "#{f["id"]} should not be required" ) if f["optional"]==false
-end
-
-# Number constraints (mins & maxs) are sensible
-descriptor["inputs"].select{ |v| v["type"] == "Number" }.each do |f|
-  min, max = f["minimum"] || -1.0/0, f["maximum"] || 1.0/0
-  errors.push( "#{f['id']} cannot have greater min (#{min}) than max (#{max})" ) if min > max
-end
-
-# List length constraints are sensible
-descriptor["inputs"].select{ |v| v["list"] }.each do |f|
-  min, max = f["min-list-entries"] || 0, f["max-list-entries"] || 1.0 / 0
-  errors.push( "#{f['id']} min list entries (#{min}) greater than max list entries (#{max})" ) if min > max
-  errors.push( "#{f['id']} cannot have negative min list entries #{min}" ) if min < 0
-  errors.push( "#{f['id']} cannot have non-positive max list entries #{max}" ) if max <= 0
-end
-
-# Enum-type inputs always have specified choices (at least 1), and the default must be in the choices set
-descriptor["inputs"].select{ |v| v["type"] == "Enum" }.each do |e|
-  errors.push( "#{e['id']} must have at least one value choice" ) if (e["enum-value-choices"] || []).length < 1
-  badDefault = (ed = e["default-value"]).nil? ? false : !((e["enum-value-choices"] || []).include? ed)
-  errors.push( "#{e['id']} cannot have an default value outside its choices" ) if badDefault
-end
-
-# Non-list inputs cannot have the min/max-list-entries property
-descriptor["inputs"].each do |v|
-  if v['list'] != true
-    errors.push( "#{v['id']} cannot specify min-list-entries as a non-list" ) if v["min-list-entries"]
-    errors.push( "#{v['id']} cannot specify max-list-entries as a non-list" ) if v["max-list-entries"]
-  end
 end
 
 ### Print the final set of errors ###
 errors << "OK" if errors == []
 puts "#{errors}"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
