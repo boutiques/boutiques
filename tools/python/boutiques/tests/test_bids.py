5ab3e2e1b39e7bff02e432fb02149f48d6eee3a0
gkiar <gkiar07@gmail.com>
2017-08-31 19:52:44 -0700

    updated schema versions for test (remember this...); fixed file import

--- tools/python/boutiques/tests/test_bids.py
+++ tools/python/boutiques/tests/test_bids.py
@@ -3,7 +3,7 @@
 from unittest import TestCase
 from boutiques.validator import validate_json, main
 from boutiques.bids import validate_bids
-from boutiques import __file__
+from boutiques import __file__ as bofile
 from jsonschema.exceptions import ValidationError
 import os.path as op
 import json
@@ -13,18 +13,18 @@ import os
 class TestBIDS(TestCase):
 
     def test_bids_good(self):
-        fil = op.join(op.split(__file__)[0], 'schema/examples/bids_good.json')
+        fil = op.join(op.split(bofile)[0], 'schema/examples/bids_good.json')
         self.assertFalse(main(args=[fil, '-b']))
 
     def test_bids_bad1(self):
-        fil = op.join(op.split(__file__)[0], 'schema/examples/bids_bad1.json')
+        fil = op.join(op.split(bofile)[0], 'schema/examples/bids_bad1.json')
         self.assertRaises(ValidationError, main, [fil, '-b'])
 
     def test_bids_bad2(self):
-        fil = op.join(op.split(__file__)[0], 'schema/examples/bids_bad2.json')
+        fil = op.join(op.split(bofile)[0], 'schema/examples/bids_bad2.json')
         self.assertRaises(ValidationError, main, [fil, '-b'])
 
     def test_bids_invalid(self):
-        fil = op.join(op.split(__file__)[0], 'schema/examples/bids_bad2.json')
+        fil = op.join(op.split(bofile)[0], 'schema/examples/bids_bad2.json')
         descriptor = json.load(open(fil))
         self.assertRaises(ValidationError, validate_bids, descriptor, False)
--- tools/python/boutiques/tests/test_import.py
+++ tools/python/boutiques/tests/test_import.py
@@ -4,7 +4,7 @@ from unittest import TestCase
 from boutiques.validator import validate_json, main as main_validate
 from boutiques.importer import main
 from boutiques.bids import validate_bids
-from boutiques import __file__
+from boutiques import __file__ as bofile
 from jsonschema.exceptions import ValidationError
 import os.path as op
 import json
@@ -14,7 +14,7 @@ import os
 class TestImport(TestCase):
 
     def test_import_good(self):
-        bids_app = op.join(op.split(__file__)[0], 'schema/examples/bids-apps/example')
+        bids_app = op.join(op.split(bofile)[0], 'schema/examples/bids-apps/example')
         self.assertFalse(main(args=[bids_app, 'test-import.json']))
 
     def test_import_valid(self):
