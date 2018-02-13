ff70f55ac456242dbbb236d83a4318607a0c32a8
Jérôme Charrière <je.charriere@gmail.com>
2018-02-13 12:38:57 -0500

    Heavy rework (added output identification based on output id; added MD5 checksum comparison; code cleanup)

--- tools/python/boutiques/test.py
+++ tools/python/boutiques/test.py
@@ -1,6 +1,7 @@
 import os.path
-from boutiques import bosh
+from boutiques import bosh, localExec
 from boutiques import __file__ as bfile
+import hashlib
 import pytest
 
 
@@ -9,25 +10,34 @@ def test(descriptor, test, invocation):
     print("test: %s" % test["name"])
     
     # Run pipeline.
-    exit_code, stdout, stderr = bosh("exec", "launch", descriptor, invocation.name)
-
+    exit_code, stdout, stderr = bosh(["exec", "launch", descriptor, invocation.name])
+    
     # Choose appropriate assertion scenario
     assertions = test["assertions"]
-    if (assertions.has_key("exit-code")):
+    if "exit-code" in assertions:
         assert (exit_code == assertions["exit-code"])
-	    
-    if (assertions.has_key("output-files")):
+    
+    if "output-files" in assertions:
+    
+        # Setting up a new LocalExecutor instance just for the sake of computing the list of output files path, coupled with their respective ids.
+        executor = localExec.LocalExecutor(descriptor,
+                                 {"forcePathType"      : True})
+        outputted = executor.getOutputFiles(invocation.name)
+        
         for output_file in assertions["output-files"]:
-        # We check the path (the path is garanteed to be here as it is required by the descriptor for an output-file)
-            target_path = output_file["path"]
-	    assert os.path.isfile(targetPath)
-	    # Addtionaly, we might have included a reference.
-	    # In this case we need to compare the inside of the reference file with the content of the outputted file.
-	    reference_path = output_file["reference"]
-	    # (We assume that the reference file exist)
-            # Do comparison using md5
-	    reference_content = open(referencePath, 'r').read()
-	    target_content = open(targetPath, 'r').read()
-	    # Proceed to the content comparaison.
-	    assert referenceContent == targetContent
             
+            file_path = outputted[output_file["id"]]
+            assert os.path.exists(file_path)
+            
+            # Optionaly, a reference may have been specified
+            if "reference" in output_file:
+                
+                reference_path = output_file["reference"]
+                # Ensure that this property point to an existing file
+                assert os.path.exists(reference_path)
+                
+                # MD5 checksum comparaison
+                compute_md5 = lambda file_name: hashlib.md5(open(file_name,'rb').read()).hexdigest()
+                actual = compute_md5(file_path)
+                reference = compute_md5(reference_path)                
+                assert actual == reference
