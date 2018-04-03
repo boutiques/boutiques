98773498c701fc9f611e01735150472ae1556454
Tristan Glatard <tristan.glatard@concordia.ca>
2018-04-03 16:18:33 -0400

    linted evaluate.py

--- tools/python/boutiques/evaluate.py
+++ tools/python/boutiques/evaluate.py
@@ -27,7 +27,7 @@
 
 def evaluateEngine(executor, query):
     try:
-        #TODO: improve splitting to not fail in valid situations
+        # TODO: improve splitting to not fail in valid situations
         layers = query.split("/")[:-1] if "/" in query else [query]
         conditions = query.split("/")[-1] if "/" in query else []
         if len(conditions) > 0:
@@ -64,12 +64,11 @@ def evaluateEngine(executor, query):
                     # print(obj)
                     query_result[obj["id"]] = executor.in_dict.get(obj["id"])
                 elif "groups" in layers:
-                    query_result[obj["id"]] = {mem : executor.in_dict.get(mem)
+                    query_result[obj["id"]] = {mem: executor.in_dict.get(mem)
                                                for mem in obj['members']}
 
         return query_result
 
-    except:
+    except Exception:
         print("Invalid query ({}). See --help.".format(query))
         return {}
-
