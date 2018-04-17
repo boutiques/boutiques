c61d536069761a88d2b1ad8cb77680d4c5e3d74c
Tristan Glatard <tristan.glatard@concordia.ca>
2018-04-17 11:24:29 -0400

    linted test code

--- tools/python/boutiques/tests/test_publisher.py
+++ tools/python/boutiques/tests/test_publisher.py
@@ -12,9 +12,9 @@ class TestPublisher(TestCase):
 
     def test_publisher(self):
         example1_dir = os.path.join(self.get_examples_dir(), "example1")
-        print(os.path.join(example1_dir,"example1.json"))
+        print(os.path.join(example1_dir, "example1.json"))
         self.assertFalse(bosh(["publish",
-                               os.path.join(example1_dir,"example1.json"),
+                               os.path.join(example1_dir, "example1.json"),
                                "Test author", "Test affiliation",
                                "--sandbox", "-y", "-v",
                                "--zenodo-token", "hAaW2wSBZMskxpfigTYHcuDrC"
