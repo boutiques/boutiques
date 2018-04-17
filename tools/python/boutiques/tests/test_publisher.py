05d6fb03b90777a1180befee00f4c6eb639cd5a9
Tristan Glatard <tristan.glatard@concordia.ca>
2018-04-17 10:52:28 -0400

    Fixed publisher test

--- tools/python/boutiques/tests/test_publisher.py
+++ tools/python/boutiques/tests/test_publisher.py
@@ -6,9 +6,13 @@ import os
 
 class TestPublisher(TestCase):
 
-    def get_boutiques_dir(self):
-        return os.path.join(os.path.split(bfile)[0], "..", "..", "..")
+    def get_examples_dir(self):
+        return os.path.join(os.path.dirname(bfile),
+                            "schema", "examples")
 
     def test_publisher(self):
-        self.assertFalse(bosh(["publish", self.get_boutiques_dir(),
-                               "test author", "example.com", "--no-github"]))
+        example1_dir = os.path.join(self.get_examples_dir(), "example1")
+        self.assertFalse(bosh(["publish",
+                               os.path.join(example1_dir,"example1.json"),
+                               "Test author", "Test affiliation",
+                               "--sandbox", "-y", "-v"]))
