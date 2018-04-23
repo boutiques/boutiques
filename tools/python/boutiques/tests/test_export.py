c62c6fa62b2d700c9f2dbfa85fc5e58b1be07acf
Tristan Glatard <tristan.glatard@concordia.ca>
2018-04-23 17:44:36 -0400

    Improved exporter test

--- tools/python/boutiques/tests/test_export.py
+++ tools/python/boutiques/tests/test_export.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+from os.path import join as opj
 from unittest import TestCase
 from boutiques import bosh
 from boutiques import __file__ as bfile
@@ -8,12 +9,16 @@ from boutiques import __file__ as bfile
 
 class TestImport(TestCase):
     def get_examples_dir(self):
-        return os.path.join(os.path.dirname(bfile),
-                            "schema", "examples")
+        return opj(os.path.dirname(bfile),
+                   "schema", "examples")
 
     def test_export(self):
-        example1_dir = os.path.join(self.get_examples_dir(), "example1")
-        example1_desc = os.path.join(example1_dir, "example1_docker.json")
+        example1_dir = opj(self.get_examples_dir(), "example1")
+        example1_desc = opj(example1_dir, "example1_docker.json")
         fout = "test-example1-carmin.json"
-        self.assertFalse(bosh(["export", "carmin", example1_desc, "123", fout]))
+        bosh(["export", "carmin", example1_desc, "123", fout])
+        ref_name = "example1_docker_exported.json"
+        assert(open(fout, "r").read().strip() == open(opj(example1_dir,
+                                                          ref_name),
+                                                      "r").read().strip())
         os.remove(fout)
