882d0f905183d527259251bd1bc2f5280f9197d1
Tristan Glatard <tristan.glatard@concordia.ca>
2018-04-24 12:56:33 -0400

    Fixed ordering of keys in JSON output. Removed spurious license headers

--- tools/python/boutiques/exporter.py
+++ tools/python/boutiques/exporter.py
@@ -1,29 +1,5 @@
 #!/usr/bin/env python
 
-# Copyright 2015 - 2017:
-#   The Royal Institution for the Advancement of Learning McGill University,
-#   Centre National de la Recherche Scientifique,
-#   University of Southern California,
-#   Concordia University
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
 import json
 import os
 import uuid
@@ -107,4 +83,4 @@ class Exporter():
             carmin_desc['errorCodesAndMessages'].append(obj)
 
         with open(output_file, 'w') as fhandle:
-            fhandle.write(json.dumps(carmin_desc, indent=4))
+            fhandle.write(json.dumps(carmin_desc, indent=4, sort_keys=True))
