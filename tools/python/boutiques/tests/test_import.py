c2b30c4d07b549134b98e3fb9486eb4ff55e2e41
Tristan Glatard <tristan.glatard@concordia.ca>
2018-04-24 09:11:30 -0400

    Fixed import test

--- tools/python/boutiques/tests/test_import.py
+++ tools/python/boutiques/tests/test_import.py
@@ -4,9 +4,10 @@ from unittest import TestCase
 from boutiques import bosh
 from boutiques import __file__ as bfile
 from jsonschema.exceptions import ValidationError
-import os.path as op
 import json
 import os
+import os.path as op
+from os.path import join as opj
 
 
 class TestImport(TestCase):
@@ -14,7 +15,14 @@ class TestImport(TestCase):
     def test_import_bids_good(self):
         bids_app = op.join(op.split(bfile)[0],
                            "schema/examples/bids-apps/example_good")
-        self.assertFalse(bosh(["import", "bids", "test-import.json", bids_app]))
+        outfile = "test-import.json"
+        ref_name = "test-import-ref.json"
+        if op.isfile(outfile):
+                os.remove(outfile)
+        self.assertFalse(bosh(["import", "bids", outfile, bids_app]))
+        assert(open(outfile, "r").read().strip() == open(opj(bids_app,
+                                                             ref_name),
+                                                         "r").read().strip())
 
     def test_import_bids_bad(self):
         bids_app = op.join(op.split(bfile)[0],
@@ -30,5 +38,11 @@ class TestImport(TestCase):
     def test_upgrade_04(self):
         fin = op.join(op.split(bfile)[0], "schema/examples/upgrade04.json")
         fout = op.join(op.split(bfile)[0], "schema/examples/upgraded05.json")
+        ref_name = "test-import-04-ref.json"
+        if op.isfile(fout):
+                os.remove(fout)
         self.assertFalse(bosh(["import", "0.4",  fout, fin]))
+        assert(open(fout, "r").read().strip() == open(opj("schema/examples",
+                                                          ref_name),
+                                                      "r").read().strip())
         os.remove(fout)
