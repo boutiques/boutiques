ccb8e440787dfaa16e7d2b955672178555495e0f
Tristan Glatard <tristan.glatard@concordia.ca>
2018-05-23 17:38:20 -0400

    Fixed coveralls install for Python 2.6

--- tools/python/install_coveralls.sh
+++ tools/python/install_coveralls.sh
@@ -7,7 +7,11 @@ MAJOR_VER=$(python -c 'import sys; print(sys.version_info[0])')
 MINOR_VER=$(python -c 'import sys; print(sys.version_info[1])')
 if [ "${MINOR_VER}" == "6" ] && [ "${MAJOR_VER}" == "2" ]
 then
-    pip install coveralls==1.2.0
+    # Use older version of pyOpenSSL
+    pip install pyOpenSSL==17.5.0
 else
-    pip install coveralls
+    # Use latest version
+    pip install pyOpenSSL
 fi
+# Now coveralls will use the old version of pyOpenSSL when using Python 2.6
+pip install coveralls
