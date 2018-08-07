61962c7e0b709edffff8936ba42cd7cbbb3bb091
Tristan Glatard <tristan.glatard@concordia.ca>
2018-08-07 17:41:12 -0400

    CWL importer and test

--- tools/python/boutiques/tests/test_import.py
+++ tools/python/boutiques/tests/test_import.py
@@ -8,6 +8,8 @@ import json
 import os
 import os.path as op
 from os.path import join as opj
+import pytest
+from boutiques.importer import ImportError
 
 
 class TestImport(TestCase):
@@ -50,3 +52,42 @@ class TestImport(TestCase):
         assert(result == open(ref_file, "r").read().strip() or
                result == open(ref_file_p2, "r").read().strip())
         os.remove(fout)
+
+    def test_import_cwl(self):
+        ex_dir = opj(op.split(bfile)[0], "tests/cwl")
+        # These ones are supposed to crash
+        bad_dirs = ["1st-workflow", "record", "array-inputs",
+                    "createfile", "expression", "nestedworkflows",
+                    "arguments"]
+        for d in os.listdir(ex_dir):
+                if d == "README.md":
+                        continue
+                files = os.listdir(opj(ex_dir, d))
+                cwl_descriptor = None
+                cwl_invocation = None
+                for f in files:
+                        if op.basename(f).endswith(".cwl"):
+                                cwl_descriptor = op.abspath(opj(ex_dir, d, f))
+                        if op.basename(f).endswith(".yml"):
+                                cwl_invocation = op.abspath(opj(ex_dir, d, f))
+                assert(cwl_descriptor is not None)
+                print(d)  # to help with debugging
+                out_desc = "./cwl_out.json"
+                out_inv = "./cwl_inv_out.json"
+                if cwl_invocation is not None:
+                        args = ["import",
+                                "cwl",
+                                out_desc,
+                                cwl_descriptor,
+                                "-i", cwl_invocation,
+                                "-o", out_inv]
+                else:
+                        args = ["import",
+                                "cwl",
+                                out_desc,
+                                cwl_descriptor]
+                if d in bad_dirs:
+                        with pytest.raises(ImportError):
+                                bosh(args)
+                else:
+                        self.assertFalse(bosh(args))
