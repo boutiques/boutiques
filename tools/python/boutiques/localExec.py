fa55d96ec5143d0e6cff132acdc46547ca1f5c7c
Erin Benderoff <ebenderoff@localhost.localdomain>
2018-09-12 13:02:03 -0400

    modified json schema to include container opts, localExec appends opts to docker run, added sample opts to example file

--- tools/python/boutiques/localExec.py
+++ tools/python/boutiques/localExec.py
@@ -219,6 +219,7 @@ class LocalExecutor(object):
         # Check for Container image
         conType, conImage = con.get('type'), con.get('image'),
         conIndex = con.get("index")
+        conOpts = con.get("container-opts")
         conIsPresent = (conImage is not None)
         # Export environment variables, if they are specified in the descriptor
         envVars = {}
@@ -294,6 +295,11 @@ class LocalExecutor(object):
             if launchDir is None:
                 launchDir = op.realpath('./')
             launchDir = op.realpath(launchDir)
+            # Get the container options
+            conOptsString = ""
+            if conOpts:
+                for opt in conOpts:
+                    conOptsString += opt + ' '
             # Run it in docker
             mount_strings = [] if not mount_strings else mount_strings
             mount_strings = [op.realpath(m.split(":")[0])+":"+m.split(":")[1]
@@ -318,6 +324,7 @@ class LocalExecutor(object):
                                      ' --rm' + envString +
                                      ' -v ' + docker_mounts +
                                      ' -w ' + launchDir + ' ' +
+                                     conOptsString + 
                                      str(conImage) + ' ' + dsname)
             elif conType == 'singularity':
                 envString = ""
