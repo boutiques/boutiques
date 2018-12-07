1515293ee84ffb3c56b31a1e95f4a4dee9864569
Erin Benderoff <erin.benderoff@gmail.com>
2018-12-06 22:34:18 -0500

    search results are sorted by number of downloads, added test cases

--- tools/python/boutiques/searcher.py
+++ tools/python/boutiques/searcher.py
@@ -1,6 +1,7 @@
 import requests
 from collections import OrderedDict
 import json
+from operator import itemgetter
 
 
 class ZenodoError(Exception):
@@ -55,15 +56,21 @@ class Searcher():
     def create_results_list(self, results):
         results_list = []
         for hit in results["hits"]["hits"]:
-            (id, title, description) = self.parse_basic_info(hit)
-            results_list.append(OrderedDict([("ID", id), ("TITLE", title),
-                                ("DESCRIPTION", description)]))
+            (id, title, description, downloads) = self.parse_basic_info(hit)
+            result_dict = OrderedDict([("ID", id), ("TITLE", title),
+                                      ("DESCRIPTION", description),
+                                      ("DOWNLOADS", downloads)])
+            if not self.no_trunc:
+                result_dict = self.truncate(result_dict, 40)
+            results_list.append(result_dict)
+        results_list = sorted(results_list, key=itemgetter('DOWNLOADS'),
+                              reverse=True)
         return results_list
 
     def create_results_list_verbose(self, results):
         results_list = []
         for hit in results["hits"]["hits"]:
-            (id, title, description) = self.parse_basic_info(hit)
+            (id, title, description, downloads) = self.parse_basic_info(hit)
             author = hit["metadata"]["creators"][0]["name"]
             version = hit["metadata"]["version"]
             doi = hit["doi"]
@@ -71,26 +78,37 @@ class Searcher():
             schema_version = keyword_data["schema-version"]
             container = keyword_data["container-type"]
             other_tags = ",".join(keyword_data["other"])
-            results_list.append(OrderedDict([
-                                ("ID", id),
-                                ("TITLE", title),
-                                ("DESCRIPTION", description),
-                                ("AUTHOR", author),
-                                ("VERSION", version),
-                                ("DOI", doi),
-                                ("SCHEMA VERSION", schema_version),
-                                ("CONTAINER", container),
-                                ("TAGS", other_tags)]))
+            result_dict = OrderedDict([("ID", id),
+                                      ("TITLE", title),
+                                      ("DESCRIPTION", description),
+                                      ("DOWNLOADS", downloads),
+                                      ("AUTHOR", author),
+                                      ("VERSION", version),
+                                      ("DOI", doi),
+                                      ("SCHEMA VERSION", schema_version),
+                                      ("CONTAINER", container),
+                                      ("TAGS", other_tags)])
+            if not self.no_trunc:
+                result_dict = self.truncate(result_dict, 40)
+            results_list.append(result_dict)
+        results_list = sorted(results_list, key=itemgetter('DOWNLOADS'),
+                              reverse=True)
         return results_list
 
     def parse_basic_info(self, hit):
-        max_descr_length = 50
         id = "zenodo." + str(hit["id"])
         title = hit["metadata"]["title"]
         description = hit["metadata"]["description"]
-        if len(description) > max_descr_length and not self.no_trunc:
-            description = description[:50] + "..."
-        return (id, title, description)
+        downloads = hit["stats"]["version_downloads"]
+        return (id, title, description, downloads)
+
+    # truncates every value of a dictionary whose length is
+    # greater than max_length
+    def truncate(self, d, max_length):
+        for k, v in list(d.items()):
+            if len(str(v)) > max_length:
+                d[k] = v[:max_length] + "..."
+        return d
 
     def print_zenodo_info(self, message, r):
         print("[ INFO ({1}) ] {0}".format(message, r.status_code))
