3b377f437109f08ee857c8e70fd65ed8c68c4fbd
sthiel <stuart.thiel@gmail.com>
2019-01-11 10:05:22 -0500

    Removed some redundant/ambigious code. Function is the same, but there is less code and intent is clearer  * I've localised the loadJson call on result.invocation in invocation() to simplify the code.  * I've in-lined the json String in test's call of invocation instead of exporting a json file and passing that.

--- tools/python/boutiques/bosh.py
+++ tools/python/boutiques/bosh.py
@@ -334,7 +334,7 @@ def invocation(*params):
                 f.write(json.dumps(descriptor, indent=4, sort_keys=True))
     if result.invocation:
         from boutiques.invocationSchemaHandler import validateSchema
-        validateSchema(invSchema, loadJson(result.invocation))
+        validateSchema(invSchema, loadJson(result.invocation)))
 
 
 def evaluate(*params):
@@ -393,10 +393,10 @@ def test(*params):
         invocation_JSON = test["invocation"]
 
         # Check if the invocation is valid.
-        invocation(result.descriptor, "--invocation",
-                   json.dumps(invocation_JSON))
+        invocation(result.descriptor, "--invocation", json.dumps(invocation_JSON).encode())
 
     # Invocations have been properly validated. We can launch the actual tests.
+
     test_path = op.join(op.dirname(op.realpath(__file__)), "test.py")
     return pytest.main([test_path, "--descriptor", result.descriptor])
 
