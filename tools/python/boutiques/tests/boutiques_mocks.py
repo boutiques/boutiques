e492cf493763a2002b72ea29673b4de12425dda7
Erin Benderoff <erin.benderoff@gmail.com>
2019-04-09 13:12:09 -0400

    updated example boutiques tool mock to the new version and moved it to the mocks file so it doesn't need to be hardcoded everywhere

--- tools/python/boutiques/tests/boutiques_mocks.py
+++ tools/python/boutiques/tests/boutiques_mocks.py
@@ -1,4 +1,4 @@
-class MockHttpResponse():
+class MockHttpResponse:
     def __init__(self, status_code, mock_json={}):
         self.status_code = status_code
         self.mock_json = mock_json
@@ -7,13 +7,21 @@ class MockHttpResponse():
         return self.mock_json
 
 
-class MockZenodoRecord():
+class MockZenodoRecord:
     def __init__(self, id, title, description="", filename="", downloads=1):
         self.id = id
         self.title = title
         self.filename = filename
         self.downloads = downloads
         self.description = description
+
+
+# Mock object representing the current version of Example Boutiques Tool
+# on Zenodo
+example_boutiques_tool = MockZenodoRecord(2634310, "Example Boutiques Tool",
+                                          "", "https://zenodo.org/api/files/"
+                                          "009cc264-76f6-459c-a824-"
+                                          "5db6241a979f/example1_docker.json")
 
 
 def mock_zenodo_test_api():
