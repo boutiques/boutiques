3945999c2aff55f55e0fe26fc4e696e7610ffa5d
Tristan Glatard <tristan.glatard@concordia.ca>
2019-05-29 17:07:21 -0400

    desc2func: function now can run 'launch' or 'simulate', improved doc, added tests

--- tools/python/boutiques/descriptor2func.py
+++ tools/python/boutiques/descriptor2func.py
@@ -3,25 +3,59 @@ from boutiques import validate, execute, prettyprint
 import json
 
 
-def function(descriptor, name=None, mode='launch'):
+def function(descriptor, name=None):
+    '''
+    Returns a function to invoke bosh.execute on a descriptor.
+    args:
+        descriptor: Zenodo id, file name, or JSON string representing a
+                    descriptor.
+        name: name of the function to create. Defaults to the tool name in the
+              descriptor.
+    '''
 
     validate(descriptor)
     descriptor_json = loadJson(descriptor)
 
     def f(*args, **kwargs):
+
+        # Set default mode to 'launch'
+        if len(args) > 0:
+            mode = args[0]
+        else:
+            mode = 'launch'
+        if mode not in ['launch', 'simulate']:
+            mode = 'launch'
+        else:
+            args = args[1:]
+
+        # Call bosh execute
         if mode == 'launch':
             return execute(mode, descriptor,
                            json.dumps(kwargs), *args)
-        elif mode == 'simulate':
+        if len(kwargs) > 0:
             return execute(mode, descriptor,
                            '-i', json.dumps(kwargs), *args)
-        else:
-            raise Exception("Unsupported mode: {}".format(mode))
+        return execute(mode, descriptor, *args)
 
     if name is None:
         f.__name__ = str(descriptor_json['name'])
     else:
         f.__name__ = name
-    f.__doc__ = prettyprint(descriptor)
+
+    # Documentation
+    doc = []
+    doc.append(r'''Runs {0} through its Boutiques interface.
+    *args:
+        - mode: 'launch' or 'simulate'. Defaults to 'launch'.
+        - other arguments: will be passed to bosh execute. Examples: '-s',
+          '-x'. See help(bosh.execute) for a complete list.
+    *kwargs:
+        {1} arguments as defined in the Boutiques descriptor, referenced
+        from input ids. Example: {2}='some_value'. See complete
+        list in descriptor help below.
+
+'''.format(f.__name__, f.__name__, descriptor_json['inputs'][0]['id']))
+    doc.append(prettyprint(descriptor))
+    f.__doc__ = ''.join(doc)
 
     return f
