eabe6fd47a46ef875df9c65d24ab0274b98423f9
Darrin <darrinfong@gmail.com>
2019-06-10 13:10:36 -0400

    Added tests for invalid invocations containing flags set to false #504

--- tools/python/boutiques/tests/test_example.py
+++ tools/python/boutiques/tests/test_example.py
@@ -23,9 +23,10 @@ class TestExample(TestCase):
 
         self.assertDictEqual({"a1": "a1",
                               "b1": "b1", "b2": "b2",
-                              "c1": "c1", "c2": "c2"}, output)
+                              "c1": "c1", "c2": "c2",
+                              "d1": True, "d2": True}, output)
 
-    def test_example(self):
+    def test_example_required_only(self):
         descriptor = op.join(op.split(bfile)[0], 'schema/examples/'
                                                  'example-invocation/'
                                                  'example_descriptor.json')
@@ -35,3 +36,20 @@ class TestExample(TestCase):
         output = json.loads(process.stdout.read())
 
         self.assertDictEqual({"b1": "b1", "c2": "c2"}, output)
+
+    def test_example_internal_validator_false_flag(self):
+        self.maxDiff = None
+        descriptor = op.join(op.split(bfile)[0], 'schema/examples/'
+                                                 'example-invocation/'
+                                                 'example_descriptor.json')
+        invocation = op.join(op.split(bfile)[0], 'schema/examples/'
+                                                 'example-invocation/'
+                                                 'example_invocation.json')
+        command = ("bosh exec simulate -i " + invocation + " " + descriptor)
+
+        process = subprocess.Popen(command, shell=True,
+                                   stderr=subprocess.PIPE)
+        error = process.stderr.read()
+
+        self.assertIn("d2 (False) flag is set to true or otherwise omitted",
+                      error)
