ed0239508a2a7e6f39131f9c8bed28e636d2eda3
Darrin <darrinfong@gmail.com>
2019-08-28 17:42:33 -0400

    unfinished attempt

---
+++ tools/python/boutiques/tests/test_requires_group.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from unittest import TestCase
+from boutiques.bosh import bosh
+import subprocess
+import os.path as op
+import simplejson as json
+
+
+class TestExample(TestCase):
+
+    def test_requires_inputs_group_valid(self):
+        pass
+
+    def test_requires_inputs_group_multiple(self):
+        pass
+
+    def test_requires_inputs_group_complex(self):
+        pass
