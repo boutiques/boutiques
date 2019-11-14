7ac141ffaa11d96b8b29f01d61af66686f22bb18
Darrin <darrinfong@gmail.com>
2019-11-14 18:28:59 -0500

    Changed theme to the default read the docs theme. Have to follow instructions on https://github.com/readthedocs/sphinx_rtd_theme/blob/master/README.rst to install theme and build docs page

--- tools/sphinx/conf.py
+++ tools/sphinx/conf.py
@@ -12,6 +12,7 @@
 #
 import os
 import sys
+import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath('../python/boutiques'))
 
 
@@ -32,7 +33,8 @@ release = 'https://github.com/boutiques/boutiques/'
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinxarg.ext'
+    'sphinxarg.ext',
+    'sphinx_rtd_theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -49,7 +51,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
