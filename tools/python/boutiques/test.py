153d8f7452cd258cd2699b211c2fadd899f5e4f1
darrinfong <darrinfong@gmail.com>
2020-04-20 19:24:45 -0400

    Changed how additional options are added to bosh test, so they can be added easily

--- tools/python/boutiques/test.py
+++ tools/python/boutiques/test.py
@@ -2,7 +2,6 @@ import os.path as op
 from boutiques import __file__ as bfile
 import boutiques as bosh
 import hashlib
-import pytest
 
 
 def compute_md5(filename):
@@ -10,10 +9,17 @@ def compute_md5(filename):
         return hashlib.md5(open(filename, 'rb').read()).hexdigest()
 
 
-def test(descriptor, test, invocation, imagepath):
+def test(descriptor, test, invocation, paramsDict):
+    arguments = ["launch", descriptor, invocation.name]
+
+    # Add any additional params to arguments
+    for flag, value in paramsDict.items():
+        arguments.append(flag)
+        if value is not None:
+            arguments.append(value)
+
+    print(arguments)
     # Run pipeline.
-    arguments = ["launch", descriptor, invocation.name,
-                 "--skip-data-collection", "--imagepath", imagepath]
     ret = bosh.execute(*arguments)
     print(ret)
 
