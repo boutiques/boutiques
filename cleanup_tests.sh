83c10282d492df390dd8c7805105ebe570278466
Darrin Fong <darrinfong@gmail.com>
2020-08-20 17:15:08 -0400

    merged upstream develop to fix localExec conflicts, updated cleanup_tests.sh

--- cleanup_tests.sh
+++ cleanup_tests.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+# Legacy script: used to clean files generated during tests
+# Now cleans temp-* files and dangling files from failed tests
 
-rm -r temp-*.sh log*.txt config*.txt example.conf stdout.txt
+rm -r temp-*.sh log*.txt config*.txt user-image.simg example.conf stdout.txt
