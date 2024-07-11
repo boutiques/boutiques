f8772ac72ba00aa034231e06ccc63fcb45e2180f
Remi Gau <remi_gau@hotmail.com>
2024-07-11 18:10:24 +0200

    try testing in CI

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
+clean:
+	rm -rf temp-*.sh log*.txt config*.txt user-image.simg example.conf stdout.txt
+
 docker_build:
 	docker build -t boutiques/example1:test ./boutiques/schema/examples/example1
 
--- tools/docker2singularity.sh
+++ tools/docker2singularity.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-docker run -v /var/run/docker.sock:/var/run/docker.sock -v ${HOME}:/output --privileged -t --rm singularityware/docker2singularity boutiques/example1:test
+docker run \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v ${HOME}:/output \
+    --privileged -t --rm \
+    singularityware/docker2singularity boutiques/example1:test
 IMGNAME=$(ls $HOME/boutiques_example1_test*.simg)
-mv ${IMGNAME} ./boutiques-example1-test.simg
+mv --verbsose ${IMGNAME} ./boutiques-example1-test.simg
